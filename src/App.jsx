2955d7f320474c62a000c174c9d12130437a7e76
gjlander <garrett.jlander@gmail.com>
2023-10-16 11:24:38 +0200

    translate start

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Card, CardBody } from "@nextui-org/react";
+import { Translate } from "./components/Translate";
 
 function App() {
     return (
-        <Card>
-            <CardBody className="text-center">
-                <h1 className="text-4xl">WordWander</h1>
-            </CardBody>
-        </Card>
+        <>
+            <Card>
+                <CardBody className="text-center">
+                    <h1 className="text-4xl">WordWander</h1>
+                </CardBody>
+            </Card>
+            <Translate />
+        </>
     );
 }
 
---
+++ src/components/Translate.jsx
@@ -0,0 +1,14 @@
+import { Card, CardBody } from "@nextui-org/react";
+
+export default function Translate() {
+    return (
+        <Card>
+            <CardBody>
+                <p>
+                    Make beautiful websites regardless of your design
+                    experience.
+                </p>
+            </CardBody>
+        </Card>
+    );
+}
